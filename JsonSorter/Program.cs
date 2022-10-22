70958da1a81896395b123d367d6d7062d5968009
Sabareeswaran Balasubramaniam <sabareeswaran.balasubramaniam@kovai.co>
2022-10-22 15:52:44 +0530

    Add project files.

---
+++ JsonSorter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonSorter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Initilizing JSON sorter.....");
+
+                Console.WriteLine("Enter the JSON file path");
+
+                var path = Console.ReadLine();
+
+                var json = await File.ReadAllTextAsync(path);
+
+                var sortService = new SortService();
+
+                Console.WriteLine("Sorting the JSON.....");
+
+                var sortedConfigurations = await sortService.SortAsync(json);
+
+                Console.WriteLine("Writing the sorted JSON into the same file.....");
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(sortedConfigurations, Formatting.Indented));
+
+                Console.WriteLine("Sorting completed.....");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ JsonSorter/SortService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonSorter
+{
+    public class SortService
+    {
+        public async Task<Dictionary<string, object>> SortAsync(string json)
+        {
+            var configurations = JsonConvert
+                .DeserializeObject<Dictionary<string, object>>(json);
+
+            var sortedConfigurations = configurations
+                .OrderBy(_ => _.Key)
+                .ToDictionary(_ => _.Key, _ => _.Value);
+
+            foreach (var configuration in configurations)
+            {
+                try
+                {
+                    var nestedConfig = await this.SortAsync(configuration.Value.ToString());
+
+                    sortedConfigurations[configuration.Key] = nestedConfig;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return await Task.FromResult(sortedConfigurations);
+        }
+    }
+}
